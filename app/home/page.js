6ad06540fd3262ebcfc256cbc02b4ee66d517896
SpaaaceKid <tomrasquin244@gmail.com>
2025-07-24 13:35:47 +0200

    Re-upload du projet propre

---
+++ app/home/page.js
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-black">
+      <img
+        src="/spacekid-banner-final.jpg"
+        alt="SpaceKid AI Hero Banner"
+        className="max-w-full h-auto rounded-xl shadow-xl"
+      />
+    </main>
+  );
+}
